fc7ba7d956cc54b525f8f25a2fb3c749519ed472
aiueda27 <aiueda27@gmail.com>
2023-07-01 13:50:32 +0900

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { signInWithPopup } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAdx-gD1dAW7CmM77ZajUCoDZEJDXJYltw",
+  authDomain: "disneyplus-clone-c6556.firebaseapp.com",
+  projectId: "disneyplus-clone-c6556",
+  storageBucket: "disneyplus-clone-c6556.appspot.com",
+  messagingSenderId: "140935692400",
+  appId: "1:140935692400:web:5886cd2f2c376f949074b5",
+  measurementId: "G-7EGR0RC66G",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const storage = getStorage();
+
+export { auth, provider, storage, signInWithPopup };
+export default db;
